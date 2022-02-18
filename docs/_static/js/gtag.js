656e1ba6f292c74f4ac62d5cb641066a376d2ea7
Travis Illig <tillig@paraesthesia.com>
2022-02-18 13:23:52 -0800

    Using new Google Analytics.

---
+++ docs/_static/js/gtag.js
@@ -0,0 +1,5 @@
+// Google Analytics 4
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-JSM1ZLXPYR');
--- docs/conf.py
+++ docs/conf.py
@@ -112,11 +112,12 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
+# Note the Google Analytics integration is done through html_static_files and
+# html_js_files because themes don't generally have the new GA4 code.
 html_theme_options = {
     'github_button': 'true',
     'github_user': 'tillig',
-    'github_repo': 'mediacenter',
-    'analytics_id': 'UA-1285161-9'
+    'github_repo': 'mediacenter'
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -141,7 +142,12 @@ html_theme_path = [alabaster.get_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
+
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_js_files
+html_js_files = [
+  ('https://www.googletagmanager.com/gtag/js?id=G-JSM1ZLXPYR', {'async': 'async'}),
+  'js/gtag.js']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
